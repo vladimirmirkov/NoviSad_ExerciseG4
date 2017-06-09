2f461ad00b5fe14e0729b29a195802d9bf51166b
somename <someemail>
2017-06-09 21:13:22 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -90,33 +90,29 @@ public class TestBowling {
 	}
 	
 	@Test
-	public void test_SetBonus() throws BowlingException
+	public void test_SetBonusSpare() throws BowlingException
 	{
 		int firstThrow = 2;
-		int secondThrow = 6;
+		int secondThrow = 8;
 		Frame f1 = new Frame(firstThrow, secondThrow);
-		Frame f2 = new Frame(firstThrow, secondThrow);
-		Frame f3 = new Frame(firstThrow, secondThrow);
-		Frame f4 = new Frame(firstThrow, secondThrow);
-		Frame f5 = new Frame(firstThrow, secondThrow);
-		Frame f6 = new Frame(firstThrow, secondThrow);
-		Frame f7 = new Frame(firstThrow, secondThrow);
-		Frame f8 = new Frame(firstThrow, secondThrow);
-		Frame f9 = new Frame(firstThrow, secondThrow);
-		Frame f10 = new Frame(firstThrow, secondThrow);
 		BowlingGame bg = new BowlingGame();
 		bg.addFrame(f1);
-		bg.addFrame(f2);
-		bg.addFrame(f3);
-		bg.addFrame(f4);
-		bg.addFrame(f5);
-		bg.addFrame(f6);
-		bg.addFrame(f7);
-		bg.addFrame(f8);
-		bg.addFrame(f9);
-		bg.addFrame(f10);
-		
-		assertEquals(80 + bg.getBonus().getFirstThrow(), bg.score());
+		bg.setBonus(firstThrow, secondThrow);
+		assertEquals(12, f1.score() + bg.getBonus().getFirstThrow() );
+	}
+	
+	@Test
+	public void test_SetBonusStrike() throws BowlingException
+	{
+		int firstThrow = 10;
+		int secondThrow = 0;
+		Frame f1 = new Frame(firstThrow, secondThrow);
+		int firstThrow2 = 4;
+		int secondThrow2 = 6;
+		BowlingGame bg = new BowlingGame();
+		bg.addFrame(f1);
+		bg.setBonus(firstThrow2, secondThrow2);
+		assertEquals(20, f1.score() + bg.getBonus().getFirstThrow() + bg.getBonus().getSecondThrow());
 	}
 	
 	@Test (expected =  BowlingException.class)
